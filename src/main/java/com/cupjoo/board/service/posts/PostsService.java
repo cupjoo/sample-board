6c21045948be50abc1e6b33601be736f475d9e98
Unknown <cupjoo@naver.com>
2020-01-16 15:45:52 +0900

    add Post CRUD page

--- src/main/java/com/cupjoo/board/service/posts/PostsService.java
+++ src/main/java/com/cupjoo/board/service/posts/PostsService.java
@@ -2,19 +2,23 @@ package com.cupjoo.board.service.posts;
 
 import com.cupjoo.board.domain.posts.Posts;
 import com.cupjoo.board.domain.posts.PostsRepository;
+import com.cupjoo.board.web.dto.PostsListResponseDto;
 import com.cupjoo.board.web.dto.PostsResponseDto;
 import com.cupjoo.board.web.dto.PostsSaveRequestDto;
 import com.cupjoo.board.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor    // final 선언된 모든 필드의 생성자 생성
 @Service
 public class PostsService {
     private final PostsRepository postsRepository;
 
+    @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
@@ -22,14 +26,29 @@ public class PostsService {
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         posts.update(requestDto.getTitle(), requestDto.getContent());   // 추가적인 쿼리 필요 X
         return id;
     }
 
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
+    }
+
+    @Transactional(readOnly = true)
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/cupjoo/board/web/PostsApiController.java
+++ src/main/java/com/cupjoo/board/web/PostsApiController.java
@@ -17,9 +17,15 @@ public class PostsApiController {
         return postsService.save(requestDto);
     }
 
-    @PutMapping("/api/vi/posts/{id}")
+    @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
     }
 
     @GetMapping("/api/v1/posts/{id}")
